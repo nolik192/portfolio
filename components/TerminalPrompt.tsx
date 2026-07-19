e61d1b77c3f182f9c3d59eff60805424448bc840
Yury Bortsov <lagepik@gmail.com>
2026-07-19 14:59:56 +0300

    Delete TerminalPrompt component

--- components/TerminalPrompt.tsx
+++
@@ -1,8 +0,0 @@
-export function TerminalPrompt({ command }: { command: string }) {
-  return (
-    <p className="font-mono text-sm text-accent-secondary mb-4">
-      <span aria-hidden="true">$ </span>
-      {command}
-    </p>
-  );
-}
